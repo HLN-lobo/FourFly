073ffc84ac3ec3764a90178307f740746d81924d
Henrique Lobo <98746888+HLN-lobo@users.noreply.github.com>
2022-09-30 09:45:12 -0300

    Update conexao.php

--- Login_ADM/conexao.php
+++ Login_ADM/conexao.php
@@ -1,14 +1,12 @@
 <?php
 
-$servidor = "localhost";
-$usuario = "root";
-$senha = "";
+$servidor = "sql208.byethost18.com";
+$usuario = "b18_32695062";
+$senha = "henrique1307";
 $dbname = "aeroporto";
 
 //Criando a conexão com o banco de dados
 
 $conn = mysqli_connect($servidor, $usuario, $senha, $dbname);
-
-
 
 ?>
